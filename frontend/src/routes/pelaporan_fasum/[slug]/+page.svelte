9c550d19f04db616b471e752206f90ece1f111e6
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2026-04-30 22:32:35 +0800

    make a slug folder and the route

---
+++ frontend/src/routes/pelaporan_fasum/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    // import PelayananHeader from '$lib/components/pelayanan_publik/PelayananHeader.svelte';
+    // import { page } from '$app/stores';
+    // import { Megaphone, X, Camera, ImagePlus } from 'lucide-svelte';
+
+    // const slug = $derived($page.params.slug || '');
+    // const namaFasilitas = $derived(
+    //     slug.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    // );
+
+
+</script>
