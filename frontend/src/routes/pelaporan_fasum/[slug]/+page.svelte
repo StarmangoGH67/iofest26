db19f5e9065c76e1b3f03660ee4cf43790ab3488
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2026-05-01 22:25:04 +0800

    still progressing

--- frontend/src/routes/pelaporan_fasum/[slug]/+page.svelte
+++ frontend/src/routes/pelaporan_fasum/[slug]/+page.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
-    // import PelayananHeader from '$lib/components/pelayanan_publik/PelayananHeader.svelte';
-    // import { page } from '$app/stores';
-    // import { Megaphone, X, Camera, ImagePlus } from 'lucide-svelte';
+    import PelayananHeader from '$lib/components/pelayanan_publik/PelayananHeader.svelte';
+    import { page } from '$app/stores';
+    import { Megaphone, X, Camera, ImagePlus } from 'lucide-svelte';
 
-    // const slug = $derived($page.params.slug || '');
-    // const namaFasilitas = $derived(
-    //     slug.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-    // );
+    const slug = $derived($page.params.slug || '');
+    const facilityName = $derived(
+        slug.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    );
 
+    // let description = $state('');
+    // let location = $state('');
+
+    // let showModal = $state(false);
+    
+    // let fileInput: HTMLInputElement;
+
+    // function chooseRealPict() {
+    //     showModal = false;
+    //     fileInput.click();
+    // }
 
 </script>
+
+<!-- <svelte:head>
+    <title>Pelaporan {facilityName}</title>
+</svelte:head> -->
+
+<div></div>
