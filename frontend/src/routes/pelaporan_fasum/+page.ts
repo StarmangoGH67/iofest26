8a7d4d04597b3a796c6923d92d1598db8d5ea638
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2026-05-01 22:23:03 +0800

    act as a transfer data

---
+++ frontend/src/routes/pelaporan_fasum/+page.ts
@@ -0,0 +1,9 @@
+import { FasumHeaderData, FasumStats, FasumCategories  } from "$lib/data/pelaporan_fasilitas_umum";
+
+export const load = () => {
+    return {
+        header: FasumHeaderData,
+        stats: FasumStats,
+        categories: FasumCategories
+    };
+} 
